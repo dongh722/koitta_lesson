f9146addccc92ec334338ee8e7570f4ecab21bb7
Donghyeok Kim <dongh722@naver.com>
2019-03-08 17:55:19 +0900

    Update file3.c

--- linux/1/file3.c
+++ linux/1/file3.c
@@ -11,10 +11,10 @@ int main(void)
 	int nw;
 	char text[64] ={0,};
 	strcpy("Hellow Linux System Programming!\n",text);
-	fd = open("text.txt", O_CREAT | O_WRONLY, 0644);
+	fd = open("text.txt", O_CREAT | O_WRONLY, 0644); //text.txt 생성하고 읽고 쓰기  -rw-r--r--와 같은 권한 부여
 	printf("fd = %d\n", fd);
 	
-	nw = write(fd, text, strlen(text));
+	nw = write(fd, text, strlen(text)); //text.txt 파일에 text 문자열 쓰기
 	printf("nw = %d\n",nw);
 
 	write(fd,"\0",1);
